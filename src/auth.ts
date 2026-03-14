ac9d2a8a4d50c604e44bdff50bbade7344459136
Dave <dkha055@aucklanduni.ac.nz>
2026-03-14 19:38:52 +1300

    Feat: Migration magic link functinality to resend

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 import Credentials from "next-auth/providers/credentials";
-import Nodemailer from "next-auth/providers/nodemailer";
+import Resend from "next-auth/providers/resend";
 import bcrypt from "bcryptjs";
 import { prisma } from "./lib/prisma";
 
@@ -22,15 +22,8 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
-    Nodemailer({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: Number(process.env.EMAIL_SERVER_PORT),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
+    Resend({
+      apiKey: process.env.RESEND_API_KEY,
       from: process.env.EMAIL_FROM,
     }),
     Credentials({
